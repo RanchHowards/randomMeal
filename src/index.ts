e1a9956a997716193224fd510b0d6dc5c289a304
RanchHowards <69303624+RanchHowards@users.noreply.github.com>
2022-07-20 12:52:30 +0200

    Satisfy linter

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,6 @@ import { Meal } from "./Meal";
 const mealContainer = document?.getElementById("meal");
 const button = document.querySelector("button");
 
-button?.addEventListener("click", () => {
-  if (!button.classList.contains("more-food")) {
-    document.querySelector("body")?.classList.remove("starting-display");
-    button.classList.add("more-food");
-    button.innerText = "MORE FOOD!";
-  }
-  fetchMeal();
-});
-
-async function fetchMeal() {
-  const data = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/random.php"
-  );
-
-  const mealData = await data.json();
-  const meal: Meal = mealData.meals[0];
-  makeMeal(meal);
-}
-
 function makeMeal(meal: Meal) {
   if (mealContainer) {
     const chunkedInstructions = meal.strInstructions
@@ -30,12 +11,12 @@ function makeMeal(meal: Meal) {
       .filter((step) => !step.includes("STEP"));
 
     const ingredientsArray = [];
-    for (let i = 0; i < 21; i++) {
+    for (let i = 0; i < 21; i += 1) {
       const ingredientKey = `strIngredient${i}` as keyof Meal;
       const measureKey = `strMeasure${i}` as keyof Meal;
-      meal[ingredientKey]
-        ? ingredientsArray.push(`${meal[ingredientKey]} - ${meal[measureKey]}`)
-        : null;
+      if (meal[ingredientKey]) {
+        ingredientsArray.push(`${meal[ingredientKey]} - ${meal[measureKey]}`);
+      }
     }
 
     mealContainer.innerHTML = `
@@ -49,5 +30,23 @@ function makeMeal(meal: Meal) {
     <ol>${chunkedInstructions.map((step) => `<li>${step}</li>`).join("")}
     </ol>
     `;
-  } else alert("Couldn't find the element ID to attach this tasty meal");
+  }
 }
+
+async function fetchMeal() {
+  const data = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+
+  const mealData = await data.json();
+  const meal: Meal = mealData.meals[0];
+  makeMeal(meal);
+}
+button?.addEventListener("click", () => {
+  if (!button.classList.contains("more-food")) {
+    document.querySelector("body")?.classList.remove("starting-display");
+    button.classList.add("more-food");
+    button.innerText = "MORE FOOD!";
+  }
+  fetchMeal();
+});
